d6004450ff3046b08fe88fc2c2b13c5aac0ba7be
VD42 <vladislav.kochemaev@gmail.com>
2019-12-06 10:30:54 +0400

    [2]

--- strategy/MyStrategy.cpp
+++ strategy/MyStrategy.cpp
@@ -3,25 +3,41 @@
 #include <optional>
 #include <map>
 
-double distanceSqr(Vec2Double a, Vec2Double b) {
-  return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
-}
-
 UnitAction MyStrategy::getAction(Unit const& unit, Game const& game, Debug & debug)
 {
 	const auto distance = [&] (double x, double y) {
 		return std::abs(unit.position.x - x) + std::abs(unit.position.y - y);
 	};
 
+	const auto nearest_enemy = [&]() {
+		auto min_distance = std::numeric_limits<double>::max();
+		std::optional<std::pair<double, double>> result;
+		for (auto const& u : game.units)
+		{
+			if (u.playerId == unit.playerId)
+				continue;
+			auto const d = distance(u.position.x, u.position.y + game.properties.unitSize.y / 2.0);
+			if (d < min_distance)
+			{
+				min_distance = d;
+				result = { u.position.x, u.position.y + game.properties.unitSize.y / 2.0 };
+			}
+		}
+		return result;
+	};
+
 	const auto nearest_hp = [&] () {
 		auto min_distance = std::numeric_limits<double>::max();
 		std::optional<std::pair<double, double>> result;
+		auto const e = nearest_enemy();
 		for (auto const& l : game.lootBoxes)
 		{
 			auto const hp = std::dynamic_pointer_cast<const Item::HealthPack>(l.item);
 			if (hp == nullptr)
 				continue;
-			auto const d = distance(l.position.x, l.position.y);
+			auto d = distance(l.position.x, l.position.y);
+			if (e.has_value() && std::abs(e.value().first - l.position.x) + std::abs(e.value().second - l.position.y) < d)
+				d += 100.0;
 			if (d < min_distance)
 			{
 				min_distance = d;
@@ -53,25 +69,19 @@ UnitAction MyStrategy::getAction(Unit const& unit, Game const& game, Debug & deb
 		return result;
 	};
 
-	const auto nearest_enemy = [&] () {
-		auto min_distance = std::numeric_limits<double>::max();
-		std::optional<std::pair<double, double>> result;
-		for (auto const& u : game.units)
-		{
-			if (u.playerId == unit.playerId)
-				continue;
-			auto const d = distance(u.position.x, u.position.y + game.properties.unitSize.y / 2.0);
-			if (d < min_distance)
-			{
-				min_distance = d;
-				result = { u.position.x, u.position.y + game.properties.unitSize.y / 2.0 };
-			}
-		}
-		return result;
-	};
-
 	const auto point_of_interest = [&] () -> std::optional<std::pair<double, double>> {
-		if (unit.health < game.properties.unitMaxHealth / 2)
+		if ([&] () {
+			if (unit.health < game.properties.unitMaxHealth - game.properties.healthPackHealth / 2.0)
+				return true;
+			for (auto const& u : game.units)
+			{
+				if (u.playerId == unit.playerId)
+					continue;
+				if (unit.health < u.health)
+					return true;
+			}
+			return false;
+		}())
 		{
 			auto const hp = nearest_hp();
 			if (hp != std::nullopt)
@@ -88,21 +98,38 @@ UnitAction MyStrategy::getAction(Unit const& unit, Game const& game, Debug & deb
 
 	auto const poi = point_of_interest();
 
+	if (poi.has_value())
+	{
+		debug.draw(CustomData::Line(Vec2Float(unit.position.x, unit.position.y), Vec2Float(poi.value().first, poi.value().second), 0.1, ColorFloat(1.0, 1.0, 1.0, 0.5)));
+		debug.draw(CustomData::Rect(Vec2Float(poi.value().first, poi.value().second), Vec2Float(0.3, 0.3), ColorFloat(1.0, 1.0, 1.0, 0.5)));
+	}
+
 	UnitAction action;
 	action.plantMine = [&] () {
 		auto const e = nearest_enemy();
 		if (!e.has_value())
 			return false;
-		if (distance(e.value().first, e.value().second) < game.properties.mineExplosionParams.radius / 2.0)
+		if (distance(e.value().first, e.value().second) < game.properties.mineExplosionParams.radius)
 			return true;
 		return false;
 	}();
 	action.aim = [&] () {
 		static std::map<decltype(unit.id), decltype(action.aim)> prev_aim;
+		static std::pair<double, double> prev_pos;
 		auto const e = nearest_enemy();
 		if (!e.has_value())
 			return prev_aim[unit.id];
-		prev_aim[unit.id] = Vec2Double(e.value().first - unit.position.x, e.value().second - unit.position.y - game.properties.unitSize.y / 2.0);
+		auto delta_x = e.value().first - prev_pos.first;
+		auto delta_y = e.value().second - prev_pos.second;
+		prev_pos = e.value();
+		auto const d = distance(e.value().first, e.value().second);
+		if (unit.weapon != nullptr)
+		{
+			delta_x *= d / unit.weapon->params.bullet.speed;
+			delta_y *= d / unit.weapon->params.bullet.speed;
+		}
+		prev_aim[unit.id] = Vec2Double(e.value().first + delta_x - unit.position.x, e.value().second + delta_y - unit.position.y - game.properties.unitSize.y / 2.0);
+		debug.draw(CustomData::Rect(Vec2Float(unit.position.x + prev_aim[unit.id].x, unit.position.y + game.properties.unitSize.y / 2.0 + prev_aim[unit.id].y), Vec2Float(0.2, 0.2), ColorFloat(0.0, 1.0, 1.0, 0.5)));
 		return prev_aim[unit.id];
 	}();
 	action.velocity = [&] () {
@@ -124,11 +151,17 @@ UnitAction MyStrategy::getAction(Unit const& unit, Game const& game, Debug & deb
 		if (unit.position.x > poi.value().first && game.level.tiles[static_cast<size_t>(unit.position.x - 1)][static_cast<size_t>(unit.position.y)] == Tile::WALL)
 			return true;
 		auto const e = nearest_enemy();
-		if (e.has_value() && distance(e.value().first, e.value().second) < 5.0)
-			return true;
+		if (e.has_value())
+		{
+			debug.draw(CustomData::Log(std::to_string(distance(e.value().first, e.value().second))));
+			if (distance(e.value().first, e.value().second) < 1.8001)
+				return true;
+		}
 		return false;
 	}();
 	action.jumpDown = [&] () {
+		if (action.jump)
+			return false;
 		if (!poi.has_value())
 			return false;
 		if (poi.value().second < unit.position.y)
@@ -142,79 +175,29 @@ UnitAction MyStrategy::getAction(Unit const& unit, Game const& game, Debug & deb
 			return true;
 		return false;
 	}();
-	action.reload = false;
 	action.shoot = [&] () {
 		auto const e = nearest_enemy();
 		if (!e.has_value())
 			return false;
-		auto delta_x = e.value().first - unit.position.x;
-		auto delta_y = e.value().second - unit.position.y;
+		auto delta_x = action.aim.x;
+		auto delta_y = action.aim.y;
 		auto step_x = delta_x / 1000.0;
 		auto step_y = delta_y / 1000.0;
 		for (double i = 0.0; i < 1000.0; ++i)
-			if (game.level.tiles[static_cast<size_t>(unit.position.x + i * step_x)][static_cast<size_t>(unit.position.y + i * step_y)] == Tile::WALL)
+			if (game.level.tiles[static_cast<size_t>(unit.position.x + i * step_x)][static_cast<size_t>(unit.position.y + game.properties.unitSize.y / 2.0 + i * step_y)] == Tile::WALL)
 				return false;
+		debug.draw(CustomData::Log("SHOOT!"));
 		return true;
 	}();
-
-	/*
-
-
-  const Unit *nearestEnemy = nullptr;
-  for (const Unit &other : game.units) {
-    if (other.playerId != unit.playerId) {
-      if (nearestEnemy == nullptr ||
-          distanceSqr(unit.position, other.position) <
-              distanceSqr(unit.position, nearestEnemy->position)) {
-        nearestEnemy = &other;
-      }
-    }
-  }
-  const LootBox *nearestWeapon = nullptr;
-  for (const LootBox &lootBox : game.lootBoxes) {
-    if (std::dynamic_pointer_cast<Item::Weapon>(lootBox.item)) {
-      if (nearestWeapon == nullptr ||
-          distanceSqr(unit.position, lootBox.position) <
-              distanceSqr(unit.position, nearestWeapon->position)) {
-        nearestWeapon = &lootBox;
-      }
-    }
-  }
-  Vec2Double targetPos = unit.position;
-  if (unit.weapon == nullptr && nearestWeapon != nullptr) {
-    targetPos = nearestWeapon->position;
-  } else if (nearestEnemy != nullptr) {
-    targetPos = nearestEnemy->position;
-  }
-  debug.draw(CustomData::Log(
-      std::string("Target pos: ") + targetPos.toString()));
-  Vec2Double aim = Vec2Double(0, 0);
-  if (nearestEnemy != nullptr) {
-    aim = Vec2Double(nearestEnemy->position.x - unit.position.x,
-                     nearestEnemy->position.y - unit.position.y);
-  }
-  bool jump = targetPos.y > unit.position.y;
-  if (targetPos.x > unit.position.x &&
-      game.level.tiles[size_t(unit.position.x + 1)][size_t(unit.position.y)] ==
-          Tile::WALL) {
-    jump = true;
-  }
-  if (targetPos.x < unit.position.x &&
-      game.level.tiles[size_t(unit.position.x - 1)][size_t(unit.position.y)] ==
-          Tile::WALL) {
-    jump = true;
-  }
-  UnitAction action;
-  action.velocity = targetPos.x - unit.position.x;
-  action.jump = jump;
-  action.jumpDown = !action.jump;
-  action.aim = aim;
-  action.shoot = true;
-  action.reload = false;
-  action.swapWeapon = false;
-  action.plantMine = false;
-  */
-
-
+	action.reload = [&] () {
+		if (action.shoot)
+			return false;
+		if (unit.weapon == nullptr)
+			return false;
+		if (unit.weapon->magazine > unit.weapon->params.magazineSize / 2)
+			return false;
+		debug.draw(CustomData::Log("RELOAD!"));
+		return true;
+	}();
 	return action;
 }
